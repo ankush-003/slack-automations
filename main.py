31b6483bebaa5981c3fe2fff57636914b2f14951
Ankush H V <anshhv2003@gmail.com>
2025-06-01 20:10:16 +0530

    removed slack_bolt

--- main.py
+++ main.py
@@ -13,15 +13,15 @@ logger = logging.getLogger(__name__)
 #     signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 # )
 
+# Listens to incoming messages that contain "hello"
+# @app.message("hello")
+# def message_hello(message, say):
+#     # say() sends a message to the channel where the event was triggered
+#     say(f"Hey there <@{message['user']}>!")
+
 api = FastAPI(title="Slack Bot API", version="1.0.0")
 
 api.include_router(slack.router)
-
-# Listens to incoming messages that contain "hello"
-@app.message("hello")
-def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    say(f"Hey there <@{message['user']}>!")
 
 @api.get("/")
 def health_check():
